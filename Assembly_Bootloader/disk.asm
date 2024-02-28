7754652cee4c3320470f9e530a8b8a9c0b754299
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-02-28 00:14:05 -0800

    Update disk.asm

--- Assembly_Bootloader/disk.asm
+++ Assembly_Bootloader/disk.asm
@@ -1,1 +1,29 @@
 DISK_LOADUP:
+  pusha
+  push dx
+
+  mov ah, 0x02
+  mov al, dh
+  mov cl, 0x02
+  
+  mov ch, 0x00
+  mov dh, 0x00
+
+  int 0x13
+  jc DISK_ERROR
+
+  pop dx
+  cmp al, dh
+
+  jne SECTORS_ERROR
+  popa
+  ret
+
+DISK_ERROR:
+  jmp DISK_LOOP
+
+SECTORS_ERROR:
+  jmp DISK_LOOP
+
+DISK_LOOP:
+  jmp $
