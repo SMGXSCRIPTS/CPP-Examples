249ce60372bb4ee8c3a1b729459d38dbd571b520
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-02-28 00:08:41 -0800

    Update mbr.asm

--- Assembly_Bootloader/mbr.asm
+++ Assembly_Bootloader/mbr.asm
@@ -10,3 +10,27 @@ mov sp, bp
 
 call LOAD_KERNEL_TO_MEMORY
 call SWITCH_TO_32BIT_SYSTEMS
+
+jmp $
+
+%include "switch_32bit.asm"
+%include "disk.asm"
+%include "gdt.asm"
+
+[bits 16]
+LOAD_KERNEL_TO_MEMORY:
+  mov bx, KERNEL_OFFLOAD_CONTROL
+  mov dh, 2
+  mov dl, [BOOT_DRIVE]
+  call DISK_LOADUP
+  ret
+
+[bits 32]
+BEGIN_32BIT_SYSTEMS:
+  call KERNEL_OFFLOAD_CONTROL
+  jmp $
+
+BOOT_DRIVE db 0
+
+times 510-($-$$) db 0
+dw 0xAA55
