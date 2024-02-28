b2aa4652c9da17c51a662555d2ee078e45f104a5
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-02-28 01:32:03 -0800

    Update ports.h

--- Assembly_Bootloader/ports.h
+++ Assembly_Bootloader/ports.h
@@ -1,3 +1,14 @@
 #progma once
 
 #include <stdint.h>
+
+unsigned char PORT_BYTE_IN(uint16_t PORT)
+{
+  unsigned char RESULT;
+  asm("in %%dx, %%al" : "=a" (RESULT) : "d" (PORT));
+}
+
+void PORT_BYTE_OUT(uint16_t PORT, uint8_t DATA)
+{
+  asm("out %%al, %%dx" : : "a" (DATA), "d" (PORT));
+}
